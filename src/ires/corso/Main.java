e679c45f7e96f6260d4945b81ee15f1c62b6d83a
git-mfabiola <fabiola.majcen@gmail.com>
2021-06-03 19:17:58 +0200

    secondo esercizio: stampare il numero maggiore e l'ordine del numero

--- src/ires/corso/Main.java
+++ src/ires/corso/Main.java
@@ -8,24 +8,29 @@ public class Main {
         Scanner in;
 	    in=new Scanner(System.in);
 
-	    //Chiedo inserimento di tre numeri
+	    //Chiedo inserimento il primo numero
         System.out.println("Inserisci un numero:");
         int n1 = in.nextInt();
+        String indexn1 = "primo";
         System.out.println("Hai inserito il numero:" + n1);
 
+        //Chiedo il secondo numero
         System.out.println("Inserisci un numero:");
         int n2 = in.nextInt();
+        String indexn2 = "secondo";
         System.out.println("Hai inserito il numero:" + n2);
 
+        //Chiedo il terzo numero
         System.out.println("Inserisci un numero:");
         int n3 = in.nextInt();
+        String indexn3 = "terzo";
         System.out.println("Hai inserito il numero:" + n3);
-        if (n1>n2 && n1>n3)
-                System.out.println("questo numero è il maggiore: " + n1);
-        else
-            if (n2>n3)
-                System.out.println("questo numero è il maggiore:" +n2);
+
+        if (n1>=n2 && n1>=n3)
+                System.out.println("il " + indexn1 +" numero è il maggiore : " + n1);
+        else if (n2>=n3)
+                System.out.println("il " + indexn2 +" numero è il maggiore : " + n2);
             else
-                System.out.println("questo numero è il maggiore: " +n3);
+                System.out.println("il " + indexn3 +" numero è il maggiore : " + n3);
     }
 }
